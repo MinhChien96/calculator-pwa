622304d221d3bc91dc6fcc36ac35676c35588603
MinhChien <chiennm96@gmail.com>
2021-11-20 23:16:30 +0700

    Test

--- src/screen/Calculator/index.js
+++ src/screen/Calculator/index.js
@@ -63,7 +63,7 @@ function Calculator({
                     <Button name="%" clickHandler={handleClick} />
                     <Button name="000" clickHandler={handleClick} />
                     <Button name="Del" clickHandler={handleClick} />
-                    <Button name="777" clickHandler={handleClick} />
+                    <Button name="7" clickHandler={handleClick} />
                     <Button name="8" clickHandler={handleClick} />
                     <Button name="9" clickHandler={handleClick} />
                     <Button name="/" clickHandler={handleClick} />
